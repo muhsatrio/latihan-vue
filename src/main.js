ed4051fe293989e6feb09646b250b7e8d48c49e8
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2020-03-30 07:49:38 +0700

    done form section

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Home from './components/Home.vue';
-
-Vue.component('Home', Home);
-Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
-// new Vue({
-//   el: '#app',
-//   render: h => h(App),
-// });
+  el: '#app',
+  render: h => h(App)
+})
